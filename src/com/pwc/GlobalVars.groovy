2ba6955e1d6f95f4ffb4466c39008d21210c5d81
Sam Calamos <sam_calamos@hotmail.com>
2018-11-28 16:15:03 +1100

    first commit

---
+++ src/com/pwc/GlobalVars.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env/ groovy
+package com.pwc
+
+public class GlobalVars {
+    static String name = "Sam"
+
+    // refer to this in a pipeline using:
+    //
+    // import com.pwc.GlobalVars
+    // println GlobalVars.name
+
+}
